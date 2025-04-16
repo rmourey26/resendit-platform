76f920c3a576121864437e9332b5fedef63429a2
rmourey26 <rmourey_jr@quantumone.io>
2025-04-15 20:10:04 -0400

    dialog overflow

--- components/business-card-editor.tsx
+++ components/business-card-editor.tsx
@@ -114,9 +114,9 @@ export function BusinessCardEditor({ userId, initialData, onUpdate }: BusinessCa
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             class="lucide lucide-pen h-4 w-4"
           >
             <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"></path>
