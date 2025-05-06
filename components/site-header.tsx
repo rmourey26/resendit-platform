bc1de00f2134fd22e7c71e6f2c2da2d7c27851ee
rmourey26 <rmourey_jr@quantumone.io>
2025-05-06 15:03:07 -0400

    header height

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -80,7 +80,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-green-500/20">
-      <div className="container flex h-16 items-center justify-between px-4 md:px-6">
+      <div className="container flex h-14 items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex items-center gap-2">
             <Image src="https://quantumone.b-cdn.net/resendit/resend-it-svg.svg" alt="Resend-It Logo" className="h-5 w-18 fill-currentColor" />
