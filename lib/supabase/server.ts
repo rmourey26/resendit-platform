3af87e926538c58667f361b0c6ddd120ba59ff94
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-05-19 15:21:12 -0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers"
 
 export function createServerSupabaseClient() {
   try {
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
 
     return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
       cookies: {
